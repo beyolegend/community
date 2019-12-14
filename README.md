86537337f475bfeac7b85fa112ff11b350b59219
jinchen <544509466@qq.com>
2019-12-14 20:48:18 +0800

    new code

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 ## majiangshequ
 
 ## 资料
-http://spring.io/guides
-http://spring.io/guides/gs/serving-web-content/
-http://elasticsearch.cn/explore
-https://v3.bootcss.com
 
-##工具
-http://git-scm.com/download
+[Spring文档](http://spring.io/guides)
+
+[Spring Web](http://spring.io/guides/gs/serving-web-content/)
+
+[es](http://elasticsearch.cn/explore)
+
+[Bootstrap](https://v3.bootcss.com)
+
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+## 工具
+[Git](http://git-scm.com/download)  
+
+[VP](http://www.visual-paradigm.com)
---
+++ src/main/java/com/communitydemo/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package com.communitydemo.controller;
+
+import com.communitydemo.dto.AccessTokenDTO;
+import com.communitydemo.dto.GithubUser;
+import com.communitydemo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("5c220bd0713cb4361bfc");
+        accessTokenDTO.setClient_secret("38d50eff9494220f973cf5786493f47156a5ba00");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
